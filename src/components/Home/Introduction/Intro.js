187aaba135e19266eb0e1f20a487cb7b28c46d6f
Dharma Thanishq Nimmala <nimmala.d@northeastern.edu>
2024-07-13 21:23:29 -0400

    added icons

--- src/components/Home/Introduction/Intro.js
+++ src/components/Home/Introduction/Intro.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import "./Intro.css";
 
-const Intro = ({toggleTabs}) => {
+const Intro = ({ toggleTabs }) => {
+  const [showIcons, setShowIcons] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowIcons(true);
+    }, 3500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
       <section className="intro-section">
@@ -12,13 +22,23 @@ const Intro = ({toggleTabs}) => {
           </h1>
           <h1 className="h1-position">Software Engineer</h1>
           <div className="intro-know-more">
-          <button className="know-more-text" onClick={()=>toggleTabs('aboutme')}> Know More</button>
+            <button className="know-more-text" onClick={() => toggleTabs('aboutme')}> Know More</button>
           </div>
         </div>
         <div className="intro-image">
           <img src="./images/IMG_4262.JPG" alt="Profile Pic"></img>
         </div>
       </section>
+      <div className={`contact-icons-container ${showIcons ? 'slide-in' : ''}`}>
+        <div className="contact-icons">
+          <a className={`${showIcons ? 'slide-in' : ''}`} href="https://github.com/NimmalaD" target="_blank" rel="noopener noreferrer">
+            <FaGithub size={40} />
+          </a>
+          <a className={`${showIcons ? 'slide-in' : ''}`} href="https://www.linkedin.com/in/dharma-thanishq/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin size={40} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
